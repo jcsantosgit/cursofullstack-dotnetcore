a0e297f4d5a162aa76314359a76eb813f0fe58eb
Joao Santos <estudoti@hotmail.com>
2021-07-24 12:47:28 -0300

    alteração do menu e navegação entre telas

--- fullstackdotnet-app/src/app/app-routing.module.ts
+++ fullstackdotnet-app/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { EventoEditComponent } from './evento/evento-edit/evento-edit.component';
 import { NotFoundComponent } from './status-code/app-notfound-component';
 import { EventoCreateComponent } from './evento/evento-create/evento-create.component';
 import { PainelComponent } from './painel/painel/painel.component';
@@ -9,6 +10,7 @@ const routes: Routes = [
   { path: '', component: PainelComponent},
   { path: 'eventos', component: EventoListComponent },
   { path: 'evento-create', component: EventoCreateComponent},
+  { path: 'evento-edit/:id', component: EventoEditComponent},
   { path: '**', component: NotFoundComponent }
 ];
 
